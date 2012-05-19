5a40f5bcbe78ca38da9aa67887018ba749c5132a
Pete Douma <pete.douma@gmail.com>
2012-05-19 11:23:43 -0400

    Fix cron

--- apps/imap/mail.py
+++ apps/imap/mail.py
@@ -1,11 +1,10 @@
 import django_header
 import settings
 
-import pdb
-import os, sys, time, re
+import time, re
 
 import logging
-logger = logging.getLogger('django.request')
+LOGGER = logging.getLogger('django.request')
 
 from os.path                    import join, split
 from PIL                        import Image
@@ -13,39 +12,41 @@ from PIL                        import Image
 EMAIL  = 'fish@coastalflyrodders.com'
 PHOTOS = join(settings.MEDIA_ROOT, 'photos/')
 
-from django.db.models           import Q
-from django.core.mail           import send_mail,EmailMessage,EmailMultiAlternatives
+from django.core.mail           import EmailMessage, EmailMultiAlternatives
 from django.contrib             import auth
-from django.contrib.auth.models import User,Message,Group
+from django.contrib.auth.models import User
 from django.template            import loader, Context
 
 # Import local stuff
-from imap                       import *
-from base.models                import *
-from friends.models             import *
-from notification.models        import *
+from imap                       import MailBox
+from base.models                import SpotUser, SpotPhoto, SpotMessage, SpotGroup
+from friends.models             import Friendship
+from notification.models        import notice_send
 from pycron                     import pycron
 
 # Pre-compiled regular expressions
 re_mobile = re.compile('[0-9]{10}')
 re_email  = re.compile('^[\w\.=-]+@[\w\.-]+\.[\w]{2,3}$')
 
-import password
 
-# This is essentially send_mail, but adds attachments.
 def mail_to(subject, message, from_email, recipient_list, attachments ):
-
+    """ This is send_mail, but adds attachments
+    """
     mail = EmailMessage(subject, message, from_email, recipient_list )
     for attach in attachments:
         mail.attach_file(attach)
     mail.send()
 
 def cronjob(job):
-    if len(job) == 0:
+    """ Check if the cron timer went off
+        job is the string of the job name
+    """
+    if len(job) == 0 or job[0] != 'fish':
         return
-    
+
     print 'Mailing @' + time.strftime("%I:%M:%S %p", time.localtime()) 
     
+    # For now only send to me
     qry = User.objects.filter(username = 'CrabbyPete')# make this ()
     spam = []
  
@@ -73,47 +74,53 @@ def cronjob(job):
         msg.attach_alternative(html, "text/html")
         try:
             msg.send(fail_silently = False)
-        except Exception, e:
-            print 'Email error: %s'% str(e)
-
+        except Exception, error:
+            err_msg =  'Email error: %s'% str(error)
+            print err_msg
+            LOGGER.error(err_msg)
     return
         
 
 def email_parts( mailbox, msg_num, em ):
+    """ Get the message, text, html,and media parts
     """
-    Get the message, text,html,and media
-    """
-    text,html  = mailbox.get_text(msg_num)
+    text, html  = mailbox.get_text(msg_num)
     
-    tag,domain = em.split('@')
+    tag, domain = em.split('@')
     media = mailbox.get_media( msg_num, prefix = tag+'-',  path = PHOTOS )
     
     if text:
         if isinstance(text, list):
-            if isinstance(text[0],list):
+            if isinstance(text[0], list):
                 text = text[0]
                         
             text = ' '.join(text)
  
             # Dump the reply part of the message
             if '----- Original Message -----' in text:
-                text,dmp = text.split('----- Original Message -----')
+                text, dmp = text.split('----- Original Message -----')
  
-    return text,html,media
+    return text, html, media
 
-
-PHOTO_TYPES = ['jpg','png','gif']      
+PHOTO_TYPES = ['jpg', 'png', 'gif']      
 def main():
-    # Get the mail
+    """ Checks mailbox and sends valid messages to the database
+    """
     mailbox = MailBox()
-    mailbox.login('douma','fishf00l')
+    mailbox.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
     
     # Set the cron job to send mail every week Mon at 12:00AM
-    cron = pycron(latency = 30)
-    #cron.add_job('* 0 0 0 0 0', 'anyfish')
-    cron.add_job('0 0 0 * * *', 'anyfish')
+    cron = pycron(latency = 240)
+    jobs = cron.add_job(' 0 0 0 * * * ', 'fish','fish')
+#    jobs = cron.add_job(' 0 * * * * * ', 'fish','anyfish')
+    if not jobs:
+        print "Cron job not added"
+    else:
+        print "Cron jobs"
+        cron.print_jobs()
+    # When this goes off string 'fish' will be passed to cron goes off @ 12:00AM
+ 
     
-
     # Continually poll
     while True:
 
@@ -122,12 +129,11 @@ def main():
         if not mail_msgs or len(mail_msgs) == 0:
             mailbox.kill()
             time.sleep(30)
-            cronjob( cron.get_matched_jobs() )
+            jobs = cron.get_matched_jobs()
+            cronjob( jobs )
             continue
 
         # Get each message
-        
-        
         for msg in mail_msgs:
             msg_num = mailbox.fetch(msg)
             em, sentby, group = mailbox.header(msg_num)
@@ -181,7 +187,7 @@ def main():
                     
                     # Just get the file name and the relative url
                     path, title = split(img)
-                    path = join('photos//',title)
+                    path = join('photos//', title)
                     photo = SpotPhoto( image = path, title = title )
                     
                     # Get the exif data before you adjust the photo
@@ -211,25 +217,27 @@ def main():
                             elif '180' in rotate:
                                 ms = ms.rotate(180) 
                         
-                        size = 400,400
+                        size = 400, 400
                         ms.thumbnail(size, Image.ANTIALIAS)
                         ms.save(img)
 
                     try:
                         photo.save()
-                    except Exception, e:
-                        print 'Photo %s failed to save because %s'%(title,str(e))
+                    except Exception, error:
+                        err_msg = 'Photo %s failed to save because %s'% ( title, str(error) )
+                        print err_msg
+                        LOGGER.error(err_msg)
                     else:
                         message.photos.add(photo)
                         try:
                             message.save()
-                        except Exception,e:
-                            print 'Photo %s failed to save to message because %s'%(title,str(e))
-					
+                        except Exception, error:
+                            err_msg = 'Photo %s failed to save to message because %s'% (title, str(error)) 
+                            print err_msg
+                            LOGGER.error(err_msg)
     
             # Check if this is a group message
             send_to = []
-
             if group and SpotGroup.objects.filter(name = group):
                 group = SpotGroup.objects.get(name = group)
 
@@ -237,24 +245,28 @@ def main():
                 if group.is_member(usr):
                     message.group = group
                     message.save()
+                    
                     members = group.get_members()
                     for member in members:
                         if member == usr:
                             continue
+                        
                         follow = group.follows_by(member)
                         if follow == 'email':
                             send_to.append(member.email)
+                        
                         elif follow == 'phone':
                             m_profile = member.get_profile()
                             send_to.append(m_profile.phone_addr)
-                        notice_send(usr, member, "message_for_group",{'group':group})
+                        
+                        notice_send(usr, member, "message_for_group", {'group':group})
 
             # Find my friends and send them a notice and email, or MMS
             else:
                 friends = Friendship.objects.friends_who_follow(usr)
                 for person in friends:
                     friend = person['friend']
-                    notice_send(usr, friend, "message_from_friend",{'user':friend})
+                    notice_send(usr, friend, "message_from_friend", {'user':friend})
                     f_profile = friend.get_profile()
                     if person['follow'] == 'email':
                         if friend.email != '':
@@ -266,8 +278,8 @@ def main():
 
             # Send email to everyone who is following this one
             if len(send_to) > 0:
-                subject = 'SpotBurn Message From %s %s'%( usr.first_name, usr.last_name )
-                mail_to( subject, text, EMAIL ,send_to, media )
+                subject = 'SpotBurn Message From %s %s'% ( usr.first_name, usr.last_name )
+                mail_to( subject, text, EMAIL, send_to, media )
 
             # Done with this email delete it
             mailbox.mark_deleted(msg)
